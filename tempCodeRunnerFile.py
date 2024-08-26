0d134768626ebb3b980f2cd5888d81a55919ca7d
Ayush8795 <ayumanz8795@gmail.com>
2024-08-27 00:46:17 +0530

    added files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+?src=filters-directSearch&sort=r&clusters=functionalAreaGid&functionAreaIdGid=2&functionAreaIdGid=6&functionAreaIdGid=7&functionAreaIdGid=9&functionAreaIdGid=14&jobAge=3&experience=5
